5f40122558b1e37ccaee30b1f809f895df80b48f
KhanhNguyen4999 <khanh.nguyenduy@buymed.asia>
2023-03-17 11:24:42 +0700

    Initial Commit

---
+++ SwiftBootCamp_Intermediate/SwiftBootCamp_IntermediateApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftBootCamp_IntermediateApp.swift
+//  SwiftBootCamp_Intermediate
+//
+//  Created by Thuocsi on 17/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBootCamp_IntermediateApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
